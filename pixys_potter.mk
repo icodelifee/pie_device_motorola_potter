239637b862378828e72a91c14ac281a4030fd0eb
icodelifee <mithunsmenon@gmail.com>
2018-09-22 16:40:44 +0000

    Defined BootAnim Res

--- pixys_potter.mk
+++ pixys_potter.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
